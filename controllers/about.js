49213700cb8230adeff5926d8f6385f91edcc7d5
hrithik <bhrithik04@gmail.com>
2023-01-23 12:22:53 +0530

    Changes

--- controllers/about.js
+++ controllers/about.js
@@ -3,26 +3,25 @@ const db = require('../database/models');
 module.exports = {
 
   getContent: async (req, res) => {
-//     const content = await db.about.findAll();
-//     if (content) {
-//       res.status(200).json(content);
-//     } else {
-//       res.status(404).send("NO Blogs!");
-//     }
-     console.log("oK");
+    const content = await db.about.findAll();
+    if (content) {
+      res.status(200).json(content);
+    } else {
+      res.status(404).send("NO Blogs!");
+    }  
   },
-//   postContent: async (req, res) => {
-//     const { img , content } = req.body;
-//     try {
-//       let r = await db.about.create({
-//         img,
-//         content,
-//       });
-//       return res.status(200).json(r);
-//     } 
-//     catch (err) {
-//       console.log("Error");
-//       return res.status(404).json(err);
-//     }
-//   },
+  postContent: async (req, res) => {
+    const { img , content } = req.body;
+    try {
+      let r = await db.about.create({
+        img,
+        content,
+      });
+      return res.status(200).json(r);
+    } 
+    catch (err) {
+      console.log("Error");
+      return res.status(404).json(err);
+    }
+  },
 };
